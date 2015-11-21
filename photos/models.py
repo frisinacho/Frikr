57d1743e0e554c0ab33b9476b978bac5f43f2515
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-11-21 22:21:44 +0100

    Create Photo model

--- photos/models.py
+++ photos/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Photo(models.Model):
+    name = models.CharField(max_length=150)
+    url = models.URLField()
+    description = models.TextField(blank=True, null=True, default="")
+    created_at = models.DateTimeField(auto_now_add=True)
+    modified_at = models.DateTimeField(auto_now=True)
+    

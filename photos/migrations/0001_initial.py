657139c709ccfa210eb64a64a92ff5d7892c9be9
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-11-22 12:21:30 +0100

    python manage.py makemigrations

---
+++ photos/migrations/0001_initial.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Photo',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('name', models.CharField(max_length=150)),
+                ('url', models.URLField()),
+                ('description', models.TextField(default=b'', null=True, blank=True)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('modified_at', models.DateTimeField(auto_now=True)),
+                ('license', models.CharField(max_length=3, choices=[(b'RIG', b'Copyright'), (b'LEF', b'Copyleft'), (b'CC', b'Creative Commons')])),
+            ],
+        ),
+    ]

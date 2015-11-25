f921c869b261a9ed0e46625023d4552e1f0bc1e9
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-11-25 14:12:20 +0100

    imported Photo model

--- photos/views.py
+++ photos/views.py
@@ -2,5 +2,9 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from photos.models import Photo
+
+
 def home(request):
+    photos = Photo.objects.all()
     return HttpResponse("Hola mundo!")

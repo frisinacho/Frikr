bae342fb4b8a4d34aa39415d2948aee4d8038829
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-11-28 17:16:11 +0100

    Calling a variable, not a hardcoded text

--- photos/views.py
+++ photos/views.py
@@ -2,11 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
-from photos.models import Photo
+from photos.models import Photo, PUBLIC
 
 
 def home(request):
-    photos = Photo.objects.filter(visibility='PUB').order_by('-created_at')
+    photos = Photo.objects.filter(visibility=PUBLIC).order_by('-created_at')
     context = {
         'photos_list': photos[:5]
     }

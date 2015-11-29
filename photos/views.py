44029b4f24b981cb3e18e832c873b3a8b138b9c9
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-11-29 18:28:31 +0100

    Detail view

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
 
 # Create your views here.
@@ -16,3 +16,32 @@ def home(request):
     }
 
     return render(request, 'photos/home.html', context)
+
+
+def detail(request, pk):
+    """
+    Carga la página de detalle de una foto
+    :param request: HttpRequest
+    :param pk: id de la foto
+    :return: HttpResponse
+    """
+    """
+    También se puede utilizar esta sintaxis de recuperación de un objeto:
+    try:
+        photo = Photo.object.het(pk=pk)
+    except Photo.DoesNotExist:
+        photo = None
+    except Photo.MultipleObjects:
+        photo = None
+    """
+    possible_photos = Photo.object.filter(pk=pk)
+    # JS: photo = (possible_photo.length == 1) ? possible_photo[0] : null;
+    photo = possible_photos[0] if len(possible_photos) == 1 else None
+    if photo is not None:
+        # cargar plantilla detalle
+        context = {
+            'photo': photo
+        }
+        return render(request, 'photos/detail.html', context)
+    else:
+        return HttpResponseNotFound()   # 404 not found

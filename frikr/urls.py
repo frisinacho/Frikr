7367ee736061781b953562f16dff59090d04d365
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-11-30 21:20:56 +0100

    Naming URLs

--- frikr/urls.py
+++ frikr/urls.py
@@ -19,6 +19,6 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
-    url(r'^$', 'photos.views.home'),
-    url(r'^photos/(?P<pk>[0-9]+)$', 'photos.views.detail'),
+    url(r'^$', 'photos.views.home', name='photos_home'),
+    url(r'^photos/(?P<pk>[0-9]+)$', 'photos.views.detail', name='photo_detail'),
 ]

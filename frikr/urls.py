743a06562a140cdadcbbba3c9f346f13d64f5a33
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-01 19:00:50 +0100

    Comment for a better understanding

--- frikr/urls.py
+++ frikr/urls.py
@@ -19,6 +19,9 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
 
+    # Photo URLs
     url(r'^$', 'photos.views.home', name='photos_home'),
     url(r'^photos/(?P<pk>[0-9]+)$', 'photos.views.detail', name='photo_detail'),
+
+    # User URLs
 ]

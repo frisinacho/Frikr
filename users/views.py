1273e296216928c7b35d82ccc4130e52cb88a026
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-01 19:15:38 +0100

    Logout implemented

--- users/views.py
+++ users/views.py
@@ -1,9 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import logout as django_logout
 
 # Create your views here.
 
 def login(request):
     pass
 
+
 def logout(request):
-    pass
+    if request.user.is_authenticated():
+        django_logout(request)
+    return redirect('photos_home')

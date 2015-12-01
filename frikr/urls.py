9f87db9ebf5fec0dd8c4f098ebac2fc161cd3f67
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-01 19:03:35 +0100

    bug fixed

--- frikr/urls.py
+++ frikr/urls.py
@@ -24,6 +24,6 @@ urlpatterns = [
     url(r'^photos/(?P<pk>[0-9]+)$', 'photos.views.detail', name='photo_detail'),
 
     # User URLs
-    url(r'login$', 'users.views.login', name='users_login'),
-    url(r'logout$', 'users.views.logout', name='users_logout'),
+    url(r'^login$', 'users.views.login', name='users_login'),
+    url(r'^logout$', 'users.views.logout', name='users_logout'),
 ]

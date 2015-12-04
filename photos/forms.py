730e55afc60f3b90ace396749278780d61ddb47b
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-04 10:16:27 +0100

    Creating new Photo form

---
+++ photos/forms.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+from django import forms
+from photos.models import Photo
+
+
+class PhotoForm(forms.ModelForm):
+    """
+    Formulario para el modelo Photo
+    """
+    class Meta:
+        model = Photo

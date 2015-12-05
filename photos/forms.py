ed4421024564b93c2409807517bff7b99fc29d25
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-05 19:13:13 +0100

    Improving BADWORDS

--- photos/forms.py
+++ photos/forms.py
@@ -23,7 +23,7 @@ class PhotoForm(forms.ModelForm):
         description = cleaned_data.get('description', '')
 
         for badword in BADWORDS:
-            if badword in description:
+            if badword.lowe() in description.lower():
                 raise ValidationError('La palabra {0} no está permitida'.format(badword))
 
         # Si todo va OK, devuelvo los datos limpios/normalizados

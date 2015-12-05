76033883ac8c2ce99eb6d35a0e9c8ac128b2ba89
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-05 18:48:27 +0100

    Making LICENSES external

--- photos/settings.py
+++ photos/settings.py
@@ -6,8 +6,10 @@ COPYRIGHT = 'RIG'
 COPYLEFT = 'LEF'
 CREATIVE_COMMONS = 'CC'
 
-LICENSES = (
+DEFAULT_LICENSES = (
     (COPYRIGHT, 'Copyright'),
     (COPYLEFT, 'Copyleft'),
     (CREATIVE_COMMONS, 'Creative Commons')
 )
+
+LICENSES = getattr(settings, 'LICENSES', DEFAULT_LICENSES)

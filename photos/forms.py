59a183034eda5994bc04096d1b7f9228401a5572
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-07 00:07:46 +0100

    Minor bug fixed

--- photos/forms.py
+++ photos/forms.py
@@ -18,12 +18,12 @@ class PhotoForm(forms.ModelForm):
         Valida si en la descripción se han puesto tacos definidos en settings.BADWORDS
         :return: diccionario con los atributos si OK
         """
-        cleaned_data = super(PhotoForm, self.clean())
+        cleaned_data = super(PhotoForm, self).clean()
 
         description = cleaned_data.get('description', '')
 
         for badword in BADWORDS:
-            if badword.lowe() in description.lower():
+            if badword.lower() in description.lower():
                 raise ValidationError(u'La palabra {0} no está permitida'.format(badword))
 
         # Si todo va OK, devuelvo los datos limpios/normalizados

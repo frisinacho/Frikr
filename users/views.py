fd7cb17d7a67010030dedfd832cf2469fe2e7e96
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-07 15:40:28 +0100

    Changing Login View

--- users/views.py
+++ users/views.py
@@ -4,32 +4,35 @@ from django.contrib.auth import logout as django_logout, authenticate, login as 
 
 # Create your views here.
 from users.forms import LoginForm
+from django.views.generic import View
 
 
-def login(request):
-    error_messages = []
-    if request.method == 'POST':
-        form = LoginForm(request.POST)
-        if form.is_valid():
-            username =form.cleaned_data.get('usr')
-            password =form.cleaned_data.get('pwd')
-            user = authenticate(username=username, password=password)
-            if user is None:
-                error_messages.append('Nombre de usuario o contraseña incorrectos')
-            else:
-                if user.is_active:
-                    django_login(request, user)
-                    url = request.GET.get('next', 'photos_home')
-                    return redirect(url)
+class LoginView(View):
+
+    def login(request):
+        error_messages = []
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                username =form.cleaned_data.get('usr')
+                password =form.cleaned_data.get('pwd')
+                user = authenticate(username=username, password=password)
+                if user is None:
+                    error_messages.append('Nombre de usuario o contraseña incorrectos')
                 else:
-                    error_messages.append('El usuario no está activo')
-    else:
-        form = LoginForm()
-    context = {
-        'errors': error_messages,
-        'login_form': form,
-    }
-    return render(request, 'users/login.html', context)
+                    if user.is_active:
+                        django_login(request, user)
+                        url = request.GET.get('next', 'photos_home')
+                        return redirect(url)
+                    else:
+                        error_messages.append('El usuario no está activo')
+        else:
+            form = LoginForm()
+        context = {
+            'errors': error_messages,
+            'login_form': form,
+        }
+        return render(request, 'users/login.html', context)
 
 
 def logout(request):

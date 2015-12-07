a01107490cc8625098c5abe74c935d9a52482102
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-07 15:42:33 +0100

    Turning Login into a get

--- users/views.py
+++ users/views.py
@@ -9,25 +9,9 @@ from django.views.generic import View
 
 class LoginView(View):
 
-    def login(request):
+    def get(self, request):
         error_messages = []
-        if request.method == 'POST':
-            form = LoginForm(request.POST)
-            if form.is_valid():
-                username =form.cleaned_data.get('usr')
-                password =form.cleaned_data.get('pwd')
-                user = authenticate(username=username, password=password)
-                if user is None:
-                    error_messages.append('Nombre de usuario o contraseña incorrectos')
-                else:
-                    if user.is_active:
-                        django_login(request, user)
-                        url = request.GET.get('next', 'photos_home')
-                        return redirect(url)
-                    else:
-                        error_messages.append('El usuario no está activo')
-        else:
-            form = LoginForm()
+        form = LoginForm()
         context = {
             'errors': error_messages,
             'login_form': form,

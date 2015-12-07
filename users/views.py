9377746155ba2f9a32d6671e27f8578431696446
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-07 15:40:56 +0100

    Duplicating view (later will be needed)

--- users/views.py
+++ users/views.py
@@ -34,6 +34,31 @@ class LoginView(View):
         }
         return render(request, 'users/login.html', context)
 
+    def login(request):
+        error_messages = []
+        if request.method == 'POST':
+            form = LoginForm(request.POST)
+            if form.is_valid():
+                username =form.cleaned_data.get('usr')
+                password =form.cleaned_data.get('pwd')
+                user = authenticate(username=username, password=password)
+                if user is None:
+                    error_messages.append('Nombre de usuario o contraseña incorrectos')
+                else:
+                    if user.is_active:
+                        django_login(request, user)
+                        url = request.GET.get('next', 'photos_home')
+                        return redirect(url)
+                    else:
+                        error_messages.append('El usuario no está activo')
+        else:
+            form = LoginForm()
+        context = {
+            'errors': error_messages,
+            'login_form': form,
+        }
+        return render(request, 'users/login.html', context)
+
 
 def logout(request):
     if request.user.is_authenticated():

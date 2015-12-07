476b1bbb7da79ebf895958b735c951fda576479f
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-08 00:45:05 +0100

    Added CreateView class

--- frikr/urls.py
+++ frikr/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 
-from photos.views import HomeView, DetailView
+from photos.views import HomeView, DetailView, CreateView
 from users.views import LoginView, LogoutView
 
 urlpatterns = [
@@ -25,7 +25,7 @@ urlpatterns = [
     # Photo URLs
     url(r'^$', HomeView.as_view(), name='photos_home'),
     url(r'^photos/(?P<pk>[0-9]+)$', DetailView.as_view(), name='photo_detail'),
-    url(r'photos/new$', 'photos.views.create', name='create_photo'),
+    url(r'photos/new$', CreateView.as_view(), name='create_photo'),
 
     # User URLs
     url(r'^login$', LoginView.as_view(), name='users_login'),

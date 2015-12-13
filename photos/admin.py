bfc63d2ab9ef77181ac7c0f4d5c82c2241273c4f
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-13 19:17:22 +0100

    More filters

--- photos/admin.py
+++ photos/admin.py
@@ -7,6 +7,8 @@ from photos.models import Photo
 class PhotoAdmin(admin.ModelAdmin):
 
     list_display = ('name', 'owner', 'license', 'visibility')
+    list_filter = ('license', 'visibility')
+    search_fields = ('name', 'description')
 
 
 admin.site.register(Photo, PhotoAdmin)

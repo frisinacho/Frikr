dd86d64df8652e5d5684a8a38198cbf3d964ea91
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-13 19:22:48 +0100

    More personalisation

--- photos/admin.py
+++ photos/admin.py
@@ -6,9 +6,14 @@ from photos.models import Photo
 
 class PhotoAdmin(admin.ModelAdmin):
 
-    list_display = ('name', 'owner', 'license', 'visibility')
+    list_display = ('name', 'owner_name', 'license', 'visibility')
     list_filter = ('license', 'visibility')
     search_fields = ('name', 'description')
+
+    def owner_name(self, obj):
+        return obj.owner.first_name + u' ' + obj.owner.last_name
+    owner_name.short_description = u'Photo owner'
+    owner_name.admin_order_field = 'owner'
 
 
 admin.site.register(Photo, PhotoAdmin)

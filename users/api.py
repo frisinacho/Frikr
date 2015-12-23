6eaf1e846e7a4b83cf128ee1da107e5b64c1e313
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-23 19:52:57 +0100

    get method

--- users/api.py
+++ users/api.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+from django.contrib.auth.models import User
+from django.views.generic import View
+from users.serializers import UserSerializer
+
+
+class UserListAPI(View):
+
+    def get(self, request):
+        users = User.objects.all()
+        serializer = UserSerializer(users, many=True)

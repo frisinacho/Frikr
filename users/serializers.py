680661cfbb370c08706e5b8d68a158be623fe263
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-23 19:41:03 +0100

    create makes an update

--- users/serializers.py
+++ users/serializers.py
@@ -19,13 +19,7 @@ class UserSerializer(serializers.Serializer):
         :return: objeto User
         """
         instance = User()
-        instance.first_name = validated_data.get('first_name')
-        instance.last_name = validated_data.get('last_name')
-        instance.username = validated_data.get('username')
-        instance.email = validated_data.get('email')
-        instance.set_password(validated_data.get('password'))
-        instance.save()
-        return instance
+        return self.update(instance, validated_data)
 
     def update(self, instance, validated_data):
         """
@@ -35,3 +29,10 @@ class UserSerializer(serializers.Serializer):
         :param validated_data: diccionario con nuevos valores para el User
         :return: User actualizado
         """
+        instance.first_name = validated_data.get('first_name')
+        instance.last_name = validated_data.get('last_name')
+        instance.username = validated_data.get('username')
+        instance.email = validated_data.get('email')
+        instance.set_password(validated_data.get('password'))
+        instance.save()
+        return instance

bda99f946c86f415fb72ef3de85e987c34cc69a0
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-23 19:30:08 +0100

    create method

--- users/serializers.py
+++ users/serializers.py
@@ -10,3 +10,11 @@ class UserSerializer(serializers.Serializer):
     username = serializers.CharField()
     email = serializers.EmailField()
     password = serializers.CharField()
+
+    def create(self, validated_data):
+        """
+        Crea una instancia de User a partir de los datos de
+        validated_data que contiene valores deserializados
+        :param validated_data: Diccionario con datos de usuario
+        :return: objeto User
+        """

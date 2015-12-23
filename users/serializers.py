780a0e7a563d228de3314b7a45420ec10337ca8a
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-23 19:27:18 +0100

    Basic fields in UserSerializer

--- users/serializers.py
+++ users/serializers.py
@@ -1,3 +1,12 @@
 # -*- coding: utf-8 -*-
 
 from rest_framework import serializers
+
+class UserSerializer(serializers.Serializer):
+
+    id = serializers.ReadOnlyField()    # read only
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    username = serializers.CharField()
+    email = serializers.EmailField()
+    password = serializers.CharField()

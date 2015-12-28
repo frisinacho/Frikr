52a1ab97b4392c34e32b6742003f21c2b2bd562e
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-28 16:42:59 +0100

    validate_username

--- users/serializers.py
+++ users/serializers.py
@@ -36,3 +36,13 @@ class UserSerializer(serializers.Serializer):
         instance.set_password(validated_data.get('password'))
         instance.save()
         return instance
+
+    def validate_username(self, data):
+        """
+        Valida si existe un usuario con ese username
+        """
+        users = User.objects.filter(username=data)
+        if len(users) != 0:
+            raise serializers.ValidationError("Ya existe un usuario con ese username")
+        else:
+            return data

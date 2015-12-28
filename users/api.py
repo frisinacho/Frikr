642c8436b69e1dd71f9e4832030d8ccc80b36604
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-28 14:00:15 +0100

    Deleting unused imports

--- users/api.py
+++ users/api.py
@@ -1,11 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.contrib.auth.models import User
-from django.http import HttpResponse
-from django.views.generic import View
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from users.serializers import UserSerializer
-from rest_framework.renderers import JSONRenderer
 
 
 class UserListAPI(APIView):

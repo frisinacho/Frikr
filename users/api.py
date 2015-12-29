7115b07d0d22199459b7df9274d5e42543f5d412
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2015-12-29 21:20:14 +0100

    else 'Bad Request'

--- users/api.py
+++ users/api.py
@@ -37,3 +37,5 @@ class UserDetailAPI(APIView):
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)

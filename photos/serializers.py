f73eaf3fb8a7528188956e80c72ccd300a93fe80
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-01-13 14:10:27 +0100

    Create PhotoSerializer

--- photos/serializers.py
+++ photos/serializers.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*-
 from rest_framework import serializers
 from models import Photo
+
+
+class PhotoSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Photo

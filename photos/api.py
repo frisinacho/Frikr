bd3bbc7f8787f44e4371ff83875d502a89584b59
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-01-13 14:15:22 +0100

    Completed GET

--- photos/api.py
+++ photos/api.py
@@ -1,8 +1,13 @@
 # -*- coding: utf-8 -*-
+from photos.models import Photo
+from photos.serializers import PhotoSerializer
+from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
 class PhotoListAPI(APIView):
 
     def get(self, request):
-        pass
+        photos = Photo.objects.all()
+        serializer = PhotoSerializer(photos, many=True)
+        return Response(serializer.data)

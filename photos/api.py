505b9441f43ff18a2a2b9cd7e83b55d81e1ed2be
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-01-18 13:48:15 +0100

    Using Generic API Views

--- photos/api.py
+++ photos/api.py
@@ -1,13 +1,9 @@
 # -*- coding: utf-8 -*-
 from photos.models import Photo
 from photos.serializers import PhotoSerializer
-from rest_framework.response import Response
-from rest_framework.views import APIView
+from rest_framework.generics import ListCreateAPIView
 
 
-class PhotoListAPI(APIView):
-
-    def get(self, request):
-        photos = Photo.objects.all()
-        serializer = PhotoSerializer(photos, many=True)
-        return Response(serializer.data)
+class PhotoListAPI(ListCreateAPIView):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer

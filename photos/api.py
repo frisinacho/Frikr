6257085486ff007aceb15c8f9da2d5f45cc6eca7
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-01-18 14:01:08 +0100

    Adding GET, UPDATE and DELETE

--- photos/api.py
+++ photos/api.py
@@ -1,9 +1,14 @@
 # -*- coding: utf-8 -*-
 from photos.models import Photo
 from photos.serializers import PhotoSerializer
-from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 
 
 class PhotoListAPI(ListCreateAPIView):
+    queryset = Photo.objects.all()
+    serializer_class = PhotoSerializer
+
+
+class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer

4f76aa28a3d04775c752b48f61a9e4d71a180ce2
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-01-19 14:09:57 +0100

    PhotoForms is now validated from model validator

--- photos/models.py
+++ photos/models.py
@@ -2,6 +2,7 @@
 from django.contrib.auth.models import User
 from django.db import models
 from photos.settings import LICENSES
+from photos.validators import badwords_detector
 
 # VISIBILITY:
 PUBLIC = 'PUB'
@@ -18,7 +19,7 @@ class Photo(models.Model):
     owner = models.ForeignKey(User)
     name = models.CharField(max_length=150)
     url = models.URLField()
-    description = models.TextField(blank=True, null=True, default="")
+    description = models.TextField(blank=True, null=True, default="", validators=[badwords_detector])
     created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
     license = models.CharField(max_length=3, choices=LICENSES)

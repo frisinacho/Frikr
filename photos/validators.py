c425d79570f72187dee91a36cab335c860288e70
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-01-19 14:06:28 +0100

    Imports and refactors

--- photos/validators.py
+++ photos/validators.py
@@ -1,4 +1,6 @@
 # -*- coding: utf-8 -*-
+from django.core.exceptions import ValidationError
+from photos.settings import BADWORDS
 
 
 def badwords_detector(value):
@@ -8,8 +10,8 @@ def badwords_detector(value):
         """
 
         for badword in BADWORDS:
-            if badword.lower() in description.lower():
+            if badword.lower() in value.lower():
                 raise ValidationError(u'La palabra {0} no está permitida'.format(badword))
 
         # Si todo va OK, devuelvo los datos limpios/normalizados
-        return cleaned_data
+        return True

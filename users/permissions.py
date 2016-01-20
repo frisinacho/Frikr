0eb6b857712bf97513650f7bbb7185a1f60d82d9
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-01-20 14:16:16 +0100

    Basic Permission file

--- users/permissions.py
+++ users/permissions.py
@@ -1,1 +1,11 @@
 # -*- coding: utf-8 -*-
+from rest_framework.permissions import BasePermission
+
+
+class UserPermission(BasePermission):
+
+    def has_permission(self, request, view):
+        pass
+
+    def has_object_permission(self, request, view, obj):
+        pass

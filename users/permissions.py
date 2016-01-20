9ae0911b46884c7ed3d6f8409c234c4b47e77855
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-01-20 14:21:19 +0100

    Basic definition

--- users/permissions.py
+++ users/permissions.py
@@ -5,7 +5,16 @@ from rest_framework.permissions import BasePermission
 class UserPermission(BasePermission):
 
     def has_permission(self, request, view):
+        """
+        Define si el usuario autenticado en request.user tiene
+        permiso para realizar la acción (GET, POST, PUT o DELETE)
+        """
         pass
 
     def has_object_permission(self, request, view, obj):
+        """
+        Define si el usuario autenticado en request.user tiene
+        permiso para realizar la acción (GET, POST, PUT o DELETE)
+        sobre el objeto 'obj'
+        """
         pass

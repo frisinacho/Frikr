ce43350d214c0a61dd7e5f4ee8c8447c48c07806
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-01-20 13:33:26 +0100

    Adding Auth Permission to the API

--- photos/api.py
+++ photos/api.py
@@ -2,10 +2,12 @@
 from photos.models import Photo
 from photos.serializers import PhotoSerializer, PhotoListSerializer
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class PhotoListAPI(ListCreateAPIView):
     queryset = Photo.objects.all()
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_serializer_class(self):
         return PhotoSerializer if self.request.method == "POST" else PhotoListSerializer
@@ -14,3 +16,4 @@ class PhotoListAPI(ListCreateAPIView):
 class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):
     queryset = Photo.objects.all()
     serializer_class = PhotoSerializer
+    permission_classes = (IsAuthenticatedOrReadOnly,)

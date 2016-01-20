ba54e5b75ccb086ba0bd90535741969af5e539d2
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-01-20 13:45:09 +0100

    Returning Photos based on Auth

--- photos/api.py
+++ photos/api.py
@@ -1,16 +1,20 @@
 # -*- coding: utf-8 -*-
 from photos.models import Photo
 from photos.serializers import PhotoSerializer, PhotoListSerializer
+from photos.views import PhotosQuerySet
 from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
-class PhotoListAPI(ListCreateAPIView):
+class PhotoListAPI(PhotosQuerySet, ListCreateAPIView):
     queryset = Photo.objects.all()
     permission_classes = (IsAuthenticatedOrReadOnly,)
 
     def get_serializer_class(self):
         return PhotoSerializer if self.request.method == "POST" else PhotoListSerializer
+
+    def get_queryset(self):
+        return self.get_photos_queryset(self.request)
 
 
 class PhotoDetailAPI(RetrieveUpdateDestroyAPIView):

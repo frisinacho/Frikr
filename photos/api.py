f298dc187757ff3edd59eb0fd8f6153b3ce0f985
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-01-21 11:43:36 +0100

    Deleting unused methods and imports

--- photos/api.py
+++ photos/api.py
@@ -2,7 +2,6 @@
 from photos.models import Photo
 from photos.serializers import PhotoSerializer, PhotoListSerializer
 from photos.views import PhotosQuerySet
-from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 
@@ -29,26 +28,3 @@ class PhotoViewSet(PhotosQuerySet, ModelViewSet):
         al usuario autenticado
         """
         serializer.save(owner=self.request.user)
-
-
-class PhotoListAPI(PhotosQuerySet, ListCreateAPIView):
-    queryset = Photo.objects.all()
-    permission_classes = (IsAuthenticatedOrReadOnly,)
-
-    def get_serializer_class(self):
-        return PhotoSerializer if self.request.method == "POST" else PhotoListSerializer
-
-    def get_queryset(self):
-        return self.get_photos_queryset(self.request)
-
-    def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)
-
-
-class PhotoDetailAPI(PhotosQuerySet, RetrieveUpdateDestroyAPIView):
-    queryset = Photo.objects.all()
-    serializer_class = PhotoSerializer
-    permission_classes = (IsAuthenticatedOrReadOnly,)
-
-    def get_queryset(self):
-        return self.get_photos_queryset(self.request)

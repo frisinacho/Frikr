8b92192f3e0eb553f7825f6eb35ea57c12882e2a
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-01-23 14:26:43 +0100

    Fulfill users/urls file

---
+++ users/urls.py
@@ -0,0 +1,15 @@
+from django.conf.urls import include, url
+
+from rest_framework.routers import DefaultRouter
+from users.api import UserViewSet
+
+
+# APIRouter
+router = DefaultRouter()
+router.register(r'api/1.0/users', UserViewSet, base_name='user')
+
+
+urlpatterns = [
+    # API URLs
+    url(r'', include(router.urls)),   # Incluyo las URLs de API
+]

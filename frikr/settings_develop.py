770142553ab0f69a30cf776fcf7b632747b410e2
Ignacio Lasaosa Sáez <frisinacho@gmail.com>
2016-01-26 13:32:02 +0100

    Change a method to use only in this settings

--- frikr/settings_develop.py
+++ frikr/settings_develop.py
@@ -1,3 +1,10 @@
 # -*- coding: utf-8 -*-
 from settings import *
 
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db_DEVELOP.sqlite3'),
+    }
+}
